cc47a95a0dfe287e5e06b8e80c2031ec1ec87687
Kumar Akshay <k.akshay9721@gmail.com>
2022-04-19 01:21:57 +0530

    Fix startup script

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -3,18 +3,18 @@
 set -x
 set -eo pipefail
 
-# if ! [ -x "$(command -v psql)" ]; then
-#    echo >&2 "Error: `psql` is not installed."
-#    exit 1
-# fi
+if ! [ -x "$(command -v psql)" ]; then
+   echo >&2 "Error: `psql` is not installed."
+   exit 1
+fi
 
-# if ! [ -x "$(command -v sqlx)" ]; thens
-#    echo >&2 "Error: `sqlx` is not installed."
-#    echo >&2 "Use:"
-#    echo >&2 "    cargo install --version=0.5.7 sqlx-cli --no-default-features --features postgres"
-#    echo >&2 "to install it."
-#    exit 1
-# fi
+if ! [ -x "$(command -v sqlx)" ]; then
+   echo >&2 "Error: `sqlx` is not installed."
+   echo >&2 "Use:"
+   echo >&2 "    cargo install --version=0.5.7 sqlx-cli --no-default-features --features postgres"
+   echo >&2 "to install it."
+   exit 1
+fi
 
 
 DB_USER="${POSTGRES_USER:=postgres}"
@@ -30,14 +30,15 @@ docker run \
   -d postgres \
   postgres -N 1000
 
-# until psql -h "localhost" -U "${DB_USER}" -p "${DB_PASSWORD}" -d "postgres" -c '\q': do
-#   >&2 echo "Postgres is still unavailable - sleeping"
-#   sleep 1
-# done
+export PGPASSWORD="${DB_PASSWORD}"
+until psql -h "localhost" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
+  >&2 echo "Postgres is still unavailable - sleeping"
+  sleep 1
+done
 
 >&2 echo "Postgres is up and running on port: ${DB_PORT} - running migrations now!"
 
-export DATABASE_URL=postgres://${DB_USER}::${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}
+export DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}
 sqlx database create
 sqlx migrate run
 
